588335d019da7a10bbd6b6f0eca47a417975dabb
noviirna <novi.irnawati@gmail.com>
2019-03-15 13:41:51 +0700

    update algoritma sorting

--- phase0-week2-jumat-exercise15.js
+++ phase0-week2-jumat-exercise15.js
@@ -1,45 +1,37 @@
 function groupAnimals(animals) {
   // you can only write your code here!
   //animals.sort();
-  var marker;
-  var a;
-  var tempanimal;
-  marker =0;
-  a = 1;
-  while (marker < animals.length - 1) {
-    //untuk keluar dari looping while
-    if (a == animals.length) {
-      marker++;
-      a = marker;
+  for (var i = animals.length-1; i>=0; i--){
+    for(var j = 1; j<=i; j++){
+      if(animals[j-1]>animals[j]){
+        var temp = animals[j-1];
+        animals[j-1] = animals[j];
+        animals[j] = temp;
       }
-    if (animals[marker] > animals[a]) { // kalau mau ascending, < tinggal diganti menjadi >
-      tempanimal = animals[marker];
-      animals[marker] = animals[a];
-      animals[a] = tempanimal;
-      }
-    a++;
     }
+  }
 
   var temp = [];
+  temp.push(animals[0]);
+  
   var result = []   
+  
   for (var i = 0; i < animals.length-1; i++){
     if(animals[i][0] == animals[i+1][0]){
-      if(i==0){
-        temp.push(animals[i]);
-      }
-      temp.push(animals[i+1]);
+        temp.push(animals[i+1]);
     }
     else{
       result.push(temp);
       temp = [];
       temp.push(animals[i+1]);
     }
+
     if(i == animals.length-2){
-      result.push(temp);
+        result.push(temp);
     }
   }
 
-    //sorting isi array[i]
+  //sorting isi array[i]
   for(var i = 0; i<result.length; i++){
     var arrtemp = [];
     for(var j = result[i].length-1; j >= 0; j--){
@@ -47,20 +39,12 @@ function groupAnimals(animals) {
     }
     result[i] = arrtemp;
   }
-return result;
+  return result;
 }
-
-
-// TEST CASES
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
-
-
-
-// TEST CASES
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cicak', 'cacing'], ['kuda', 'kancil'], ['unta'] ]
+  
+  
+  // TEST CASES
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+  console.log(groupAnimals(['cacing', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
