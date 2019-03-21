93afd28caad6259cd9bc1b90cde912f5c176a137
noviirna <novi.irnawati@gmail.com>
2019-03-21 13:19:46 +0700

    revisi FPB, menambahkan tanda = untuk mencari nilai pembagi

--- phase0-week2-jumat-exercise2.js
+++ phase0-week2-jumat-exercise2.js
@@ -1,22 +1,23 @@
 function fpb(angka1, angka2) {
   // you can only write your code here!
-  var temp1 = [];
-  for(var i = 0; i < angka1; i++){
+  var pembagike1 = [];
+  for(var i = 0; i <= angka1; i++){
     if(angka1 % i == 0){
-      temp1.push(i);
-    }
-  }
-  var temp2 = [];
-  for(var i = 0; i < angka2; i++){
-    if(angka2 % i == 0){
-      temp2.push(i);
+      pembagike1.push(i);
     }
   }
 
-  for (var i = temp2.length - 1; i >= 0; i--){
-    for(var j = temp1.length - 1; j >= 0; j--){
-      if(temp1[j] == temp2[i]){
-        return temp1[j];
+  var pembagike2 = [];
+  for(var i = 0; i <= angka2; i++){
+    if(angka2 % i == 0){
+      pembagike2.push(i);
+    }
+  }
+
+  for (var i = pembagike2.length - 1; i >= 0; i--){
+    for(var j = pembagike1.length - 1; j >= 0; j--){
+      if(pembagike1[j] == pembagike2[i]){
+        return pembagike1[j];
       }
     }
   }
@@ -28,3 +29,5 @@ console.log(fpb(50, 40)); // 10
 console.log(fpb(22, 99)); // 11
 console.log(fpb(24, 36)); // 12
 console.log(fpb(17, 23)); // 1
+console.log(fpb(16, 16)); // 16
+console.log(fpb(50, 10)); // 10
